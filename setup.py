f725fcf1d7a1abc1b9c4b3ff9e9524c587050058
xzh278 <xiaoyou.zhang@nike.com>
2023-02-03 17:45:21 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='UI-Inspector'
+    name='uiinspector',
+    version='1.0.0',
+    packages=find_packages(
+        include=['uiinspector*']
+    ),
+    entry_points={
+        'console_scripts': [
+            'cli-name = uiinspector.script:run'
+        ]
+    },
+    install_requires=['PySide6==6.3.0', 'uiautomation>=2.0.16', 'pyjab>=1.1.5', 'mouse', 'keyboard'],
+    include_package_data=True
 )
 
