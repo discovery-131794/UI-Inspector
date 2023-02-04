3fe374baef1478a38b35d3f67e0d9b08506735ec
xzh278 <xiaoyou.zhang@nike.com>
2023-02-04 15:42:40 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(
         include=['uiinspector*']
     ),
     entry_points={
-        'console_scripts': [
-            'cli-name = uiinspector.script:run'
+        'gui_scripts': [
+            'ui-inspector = uiinspector.uiinspector:run'
         ]
     },
     install_requires=['PySide6==6.3.0', 'uiautomation>=2.0.16', 'pyjab>=1.1.5', 'mouse', 'keyboard'],
